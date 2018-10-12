bde246b118ebdc0e7822999b618e230d3b9c19a7
Adam Oliveras <theadamoliveras@gmail.com>
2018-10-12 13:55:58 -0700

    finished app logic, all is well. Next step, push to heroku

---
+++ server.js
@@ -0,0 +1,34 @@
+const
+    express = require('express'),
+    app = express(),
+    ejs = require('ejs'),
+    axios = require('axios'),
+    PORT = 3000
+
+const apiClient = axios.create();
+
+app.use(express.json());
+
+
+// ejs configuration
+app.set('view engine', 'ejs')
+
+
+//routes
+app.get('/', (req, res) =>{
+    res.render('index')
+})
+app.get('/posts', (req, res) =>{
+    console.log("Request recieved, constacting api...");
+    const apiUrl = "https://jsonplaceholder.typicode.com/posts";
+    apiClient({ method: "get", url: apiUrl })
+    .then(response => {
+        console.log(response.data)
+        let data = response.data
+        res.render('showPosts', { payload: data })
+    })
+})
+
+app.listen( PORT, err  => {
+                console.log(err || `Server listening on ${PORT}`)
+            })
